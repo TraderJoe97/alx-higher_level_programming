86206300bb2d424dafe5ed93b442bcf513a9e7cc
TraderJoe97 <jimonakedi@gmail.com>
2022-06-30 19:03:01 +0200

    submit and check

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from itertools import count
+
+
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv) -1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print(f"1 argument:\n{count}: {sys.argv[count]}")
+    else:
+        print(f"{count} arguments:")
+        for i in range(count):
+            print(f"{i + 1}: {sys.argv[i + 1]}")
