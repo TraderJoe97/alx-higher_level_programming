02a1512867d876c2ff2a25d8e75a071469fceb76
TraderJoe97 <jimonakedi@gmail.com>
2022-06-30 19:58:38 +0200

    calculator fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print(f"10 + 5 = {calculator_1.add(a, b)}")
-    print(f"10 - 5 = {calculator_1.sub(a, b)}")
-    print(f"10 * 5 = {calculator_1.mul(a, b)}")
-    print(f"10 / 5 = {calculator_1.div(a, b)}")
+    print(f"10 + 5 = {add(a, b)}")
+    print(f"10 - 5 = {sub(a, b)}")
+    print(f"10 * 5 = {mul(a, b)}")
+    print(f"10 / 5 = {div(a, b)}")
