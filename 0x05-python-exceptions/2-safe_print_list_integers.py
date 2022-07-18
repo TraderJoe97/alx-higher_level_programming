2151238b4aa47754c50631c20b4d971f779db699
TraderJoe97 <jimonakedi@gmail.com>
2022-07-18 18:33:28 +0200

    add indexerror Exeption

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,10 +2,11 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-    """print the first x elements of a list that are integers.
+    """Print the first x elements of a list that are integers.
     Args:
         my_list (list): The list to print elements from.
         x (int): The number of elements of my_list to print.
+
     Returns:
         The number of elements printed.
     """
@@ -14,7 +15,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             ret += 1
-        except (ValueError, TypeError):
+        except (ValueError, TypeError, IndexError):
             continue
-        print("")
-        return (ret)
+    print("")
+    return (ret)
