37b6fb93fa0ce5fec52fbf99305e9754b52f0a7c
TraderJoe97 <jimonakedi@gmail.com>
2022-07-19 18:58:08 +0200

    task 0-4 push

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-square.py
+# Joseph I Monakedi <jimonakedi@gmail.com>
+"""Define a class Square."""
+
+
+class Square:
+    """Represents a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 1-square.py
+# Joseph I Monakedi <jimonakedi>
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size):
+        """initialize a new Square.
+
+        Args:
+            Size (int): The size of the new square.
+        """
+        self.__size = size
