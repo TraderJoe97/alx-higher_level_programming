0b30cdd6b90d27573a129f87a387649fad871963
Joseph <jimonakedi@gmail.com>
2022-10-03 23:53:27 +0200

    length spelling fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 'use strict';
-if (process.argv.lenth === 2) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (process.argv.lenth === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
